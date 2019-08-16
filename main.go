422cc76702f3b95e8d0aaf946d575d2d30ab0861
Keaton Robert Gallagher <keaton@keaton.local>
2019-08-16 10:00:41 -0600

    slices, types and arrays

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
+//First lesson
+// package main
+
+// // FMT is for formating, commonly used with Println
+// import "fmt"
+
+// func main() {
+// 	// var card string = "Ace of Spades"
+// 	// Short hand for line above
+// 	card := newCard()
+// 	// := is only used when creating a new variable
+// 	fmt.Println(card)
+// }
+
+// func newCard() string { // Function decleration
+// 	return "5 of diamonds"
+// }
+
 package main
 
-func main() { // Has to be first matching package main
-	cards := newDeck()
-	cards.shuffle()
-	cards.print()
+import "fmt"
+
+func main() {
+	//Slice of type string
+	cards := []string{"Ace of diamonds", newCard()}
+	cards = append(cards, "six of spades")
+	fmt.Println(my_cards)
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
+
+// func newCard() string { // Function decleration
+// 	return my_cards
+// }
